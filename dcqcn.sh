b9437faadd669e7a593c4210485b8a73fd2739e5
Miao-er <Miao_er@outlook.com>
2025-08-09 04:07:16 +0800

    [add] modify ip

--- dcqcn.sh
+++ dcqcn.sh
@@ -7,9 +7,10 @@ else
     echo "Usage: $0 <enable|disable>"
     exit 1
 fi
-ip_list=(192.168.11.2 192.168.12.2 192.168.13.2 192.168.21.2 192.168.22.2 192.168.23.2)
+#ip_list=(192.168.11.2 192.168.12.2 192.168.13.2 192.168.21.2 192.168.22.2 192.168.23.2)
+ip_list=(10.2.229.111 10.2.229.121 10.2.229.131 10.2.152.211 10.2.152.221 10.2.152.231)
 for ip in ${ip_list[@]}; do
     sshpass -p "123456" ssh root@$ip "ls /sys/class/net/enp1s0f0np0/ecn/roce_np/enable/* | xargs -I {} sh -c 'echo $value > {}'"
     sshpass -p "123456" ssh root@$ip "ls /sys/class/net/enp1s0f0np0/ecn/roce_rp/enable/* | xargs -I {} sh -c 'echo $value > {}'"
     echo "$1 ECN for $ip"
-done
+done    
--- initall.sh
+++ initall.sh
@@ -1,4 +1,5 @@
-ip_list=(192.168.11.2 192.168.12.2 192.168.13.2 192.168.21.2 192.168.22.2 192.168.23.2)
+# ip_list=(192.168.11.2 192.168.12.2 192.168.13.2 192.168.21.2 192.168.22.2 192.168.23.2)
+ip_list=(10.2.229.111 10.2.229.121 10.2.229.131 10.2.152.211 10.2.152.221 10.2.152.231)
 user_list=(server1 server2 server3 server4 server5 server6)
 for i in ${!ip_list[@]}; do
     sshpass -p "123456" ssh ${user_list[$i]}@${ip_list[$i]} "sudo bash /home/${user_list[$i]}/fileTransferCmd/init.sh"
